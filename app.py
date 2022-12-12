ab31b7aa97179f3ae1163c99673f47b607b26f53
Danijel <31479093+ethernal12@users.noreply.github.com>
2022-12-12 17:10:06 +0100

    Adding full TODO app functionality, with add, update and delete TODO list, with semantic UI styling.

--- app.py
+++ app.py
@@ -1,6 +1,56 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    cpmplete = db.Column(db.Boolean)
+
+
 @app.route('/')
 def index():
-    return 'Hello world'
+    todo_list = Todo.query.all()
+    print(todo_list)
+    return render_template('base.html', todo_list=todo_list)
+
+
+@app.route("/add", methods=["GET","POST"])
+def add():
+    title = request.form.get("title")
+    new_todo = Todo(title=title, cpmplete=False)
+    db.session.add(new_todo)
+    db.session.commit()
+    return redirect(url_for("index"))
+
+@app.route("/update/<int:todo_id>")
+def update(todo_id):
+    todo = Todo.query.filter_by(id=todo_id).first()
+    todo.cpmplete = not todo.cpmplete
+    db.session.commit()
+    return redirect(url_for("index"))
+
+@app.route("/delete/<int:todo_id>")
+def delete(todo_id):
+    todo = Todo.query.filter_by(id=todo_id).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect(url_for("index"))
+@app.route('/About')
+def about():
+    # show all TODOS
+    return 'About'
+
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+        new_todo = Todo(title='title 1', cpmplete=False)
+        db.session.add(new_todo)
+        db.session.commit()
+    app.run(debug='True')
